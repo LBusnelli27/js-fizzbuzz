7a9b7881ccea915293ea93532aad63065f767ce4
Busna27 <lucabusnelli2001@gmail.com>
2022-02-24 17:41:33 +0100

    listener on btn to gen only 10 boxes + fix refresh btn

--- js/main.js
+++ js/main.js
@@ -1,19 +1,40 @@
+const userBtnGen = document.getElementById('my-btn-add-10');
+userBtnGen.addEventListener('click', function() {
+    for (let i = 1; i < 11; i++) {
 
-
-for (let i = 1; i < 101; i++) {
-
-    let text = i;
-    if (((i % 3) == 0) && ((i % 5) == 0)) {
-        text = 'FizzBuzz';
-    } else if ((i % 3) == 0) {
-        text = 'Fizz';
-    } else if ((i % 5) == 0) {
-        text = 'Buzz';
+        let text = i;
+        if (((i % 3) == 0) && ((i % 5) == 0)) {
+            text = 'FizzBuzz';
+        } else if ((i % 3) == 0) {
+            text = 'Fizz';
+        } else if ((i % 5) == 0) {
+            text = 'Buzz';
+        }
+    
+    
+        let box = document.createElement("div");
+        box.classList.add("col-3" , "p-4" , "text-center" , "fw-bold" , "bg-warning" , "my-box");
+        document.getElementById("my-row-boxes").append(box);
+        box.innerHTML = text;
     }
+});
+
+// ! Questo codice sotto commentato crea tutti i quadrati fino a 100
+
+// for (let i = 1; i < 101; i++) {
+
+//     let text = i;
+//     if (((i % 3) == 0) && ((i % 5) == 0)) {
+//         text = 'FizzBuzz';
+//     } else if ((i % 3) == 0) {
+//         text = 'Fizz';
+//     } else if ((i % 5) == 0) {
+//         text = 'Buzz';
+//     }
 
 
-    let box = document.createElement("div");
-    box.classList.add("col-3" , "p-4" , "text-center" , "fw-bold" , "bg-warning" , "my-box");
-    document.getElementById("my-row-boxes").append(box);
-    box.innerHTML = text;
-}
+//     let box = document.createElement("div");
+//     box.classList.add("col-3" , "p-4" , "text-center" , "fw-bold" , "bg-warning" , "my-box");
+//     document.getElementById("my-row-boxes").append(box);
+//     box.innerHTML = text;
+// }
