6d06eb78766791b961668bee17f8f1fb6846917f
Busna27 <lucabusnelli2001@gmail.com>
2022-02-24 18:38:04 +0100

    fix the count cycle for in js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
+let count = 1;
+
 const userBtnGen = document.getElementById('my-btn-add-10');
 userBtnGen.addEventListener('click', function() {
     console.log('Added 10 boxes - btn pressed');
 
-    for (let i = 1; i < 11; i++) {
+    count = count + 10;
+
+    for (let i = count - 10; i < count; i++) {
 
         let text = i;
         if (((i % 3) == 0) && ((i % 5) == 0)) {
@@ -12,8 +16,8 @@ userBtnGen.addEventListener('click', function() {
         } else if ((i % 5) == 0) {
             text = 'Buzz';
         }
-    
-    
+
+
         let box = document.createElement("div");
         box.classList.add("col-3" , "p-4" , "text-center" , "fw-bold" , "bg-warning" , "my-box");
         document.getElementById("my-row-boxes").append(box);
